70306daf3d1b5f3c05066a4b870ac245ce406d31
EL-ViiPeR <91619950+EL-ViiPeR@users.noreply.github.com>
2024-02-24 11:33:41 +0100

    index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Listening to Spotify","Watching YouTube"];
+const statusMessages = ["〃Joue à Site 88","〃En train de Modérer"];
 
 
 let currentIndex = 0;
